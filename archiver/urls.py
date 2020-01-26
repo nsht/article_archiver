9c7f792dc619d190c2349ff1ceb3f666ea2263fe
nishit <nishitmohanan@gmail.com>
2020-01-27 00:23:19 +0530

    Code cleanup

--- archiver/urls.py
+++ archiver/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("", views.Index.as_view(), name="index"),
     path("healthcheck/", views.HealthCheck.as_view(), name="healthcheck"),
     path("article/", views.Article.as_view(), name="article"),
-    path("article_list/",views.GetArticles.as_view(), name="article_list"),
+    path("article_list/", views.GetArticles.as_view(), name="article_list"),
     # user
     path("user/", views.UserCreate.as_view(), name="usercreate"),
     path("login/", views.Login.as_view(), name="login"),
